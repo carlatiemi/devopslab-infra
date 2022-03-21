e815ca03b5784248266d8cb2e36cdcff37a4892b
carlatiemi <79721114+carlatiemi@users.noreply.github.com>
2022-03-20 21:28:07 -0300

    App Engine Provision

--- providers.tf
+++ providers.tf
@@ -15,3 +15,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-b"
 }
+
+provider "google-beta" {
+  project = "labdevopscloud-6asocarla"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
